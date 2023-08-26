5b0f313d6caff43bf5017058e746f31005df2682
Neil Gevaux <neilgevaux@gmail.com>
2023-08-26 17:19:08 +0100

    Add fixtures controller and view

---
+++ app/controllers/fixtures_controller.rb
@@ -0,0 +1,16 @@
+class FixturesController < ApplicationController
+  def fixtures
+
+    require 'httparty'
+
+    url = URI("https://api-football-v1.p.rapidapi.com/v3/fixtures?league=39&season=2023")
+
+    response = HTTParty.get(url, headers: {
+      "X-RapidAPI-Key" => ENV['API_KEY'],
+      "X-RapidAPI-Host" => 'api-football-v1.p.rapidapi.com'
+    })
+
+    @fixtures = JSON.parse(response.body)
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get 'football_data/show_team_statistics'
+
+  get 'fixtures/fixtures'
 end
