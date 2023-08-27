f08e404d1ad4c365cd60db0774369c847c6a52cb
Neil Gevaux <neilgevaux@gmail.com>
2023-08-27 13:02:49 +0100

    Add relations

--- app/models/fixture.rb
+++ app/models/fixture.rb
@@ -1,2 +1,8 @@
 class Fixture < ApplicationRecord
+  belongs_to :venue
+  belongs_to :league
+  has_one :home_team, class_name: 'Team', foreign_key: 'home_fixture_id'
+  has_one :away_team, class_name: 'Team', foreign_key: 'away_fixture_id'
+  has_one :goal
+  has_one :score
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,2 +1,4 @@
 class Team < ApplicationRecord
+  belongs_to :home_fixture, class_name: 'Fixture', foreign_key: 'home_fixture_id'
+  belongs_to :away_fixture, class_name: 'Fixture', foreign_key: 'away_fixture_id'
 end
