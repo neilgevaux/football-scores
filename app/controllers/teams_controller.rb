4ace42710f73b9e17f0ed3da4cadb28a1ea9b287
Neil Gevaux <neilgevaux@gmail.com>
2023-08-27 21:31:48 +0100

    Add table to teams view

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,18 +1,18 @@
 class TeamsController < ApplicationController
   def show
 
-    require 'httparty'
+    # require 'httparty'
 
-    url = URI("https://api-football-v1.p.rapidapi.com/v3/teams?country=england")
+    # url = URI("https://api-football-v1.p.rapidapi.com/v3/teams?country=england")
 
-    response = HTTParty.get(url, headers: {
-      "X-RapidAPI-Key" => ENV['API_KEY'],
-      "X-RapidAPI-Host" => 'api-football-v1.p.rapidapi.com'
-    })
+    # response = HTTParty.get(url, headers: {
+    #   "X-RapidAPI-Key" => ENV['API_KEY'],
+    #   "X-RapidAPI-Host" => 'api-football-v1.p.rapidapi.com'
+    # })
 
-    puts response.body
+    # puts response.body
 
-    @teams = JSON.parse(response.body)
+    # @teams = JSON.parse(response.body)
 
   end
 end
